e7d8179ad070bbd1a1db1b550a2a11f0b26a850b
Luka Gedenidze <kroxasaba@gmail.com>
2025-02-15 19:08:06 +0400

    Done

--- obj/Debug/net5.0/Integrations.AssemblyInfo.cs
+++ obj/Debug/net5.0/Integrations.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Integrations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de92e63ff4b6f16e0f022280c995ee0d7ad8476e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Integrations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Integrations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
