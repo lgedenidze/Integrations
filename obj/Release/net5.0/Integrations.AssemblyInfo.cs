04c332a1ca65cc01279cfdfaaee9bd8277ed19ef
Luka Gedenidze <kroxasaba@gmail.com>
2025-02-15 19:11:39 +0400

    add to git new changes

--- obj/Release/net5.0/Integrations.AssemblyInfo.cs
+++ obj/Release/net5.0/Integrations.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Integrations")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2850d1b49f149794155183f6625c56a6de23b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Integrations")]
