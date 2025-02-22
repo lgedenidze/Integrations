96746347cec5041802e20a7b05831b8f272e09a2
Luka Gedenidze <kroxasaba@gmail.com>
2025-02-23 03:15:26 +0400

    add baskets into events

--- Services/EventRepository.cs
+++ Services/EventRepository.cs
@@ -26,6 +26,7 @@ namespace Integrations.Services
 
             return await _context.Events
                 .Include(e => e.LineUps)
+                .Include(e => e.Baskets)
                 .Where(e => e.EndDate >= cutoffTime) // ✅ Ensure the event is still active
                 .OrderBy(e => e.Id) // ✅ Order by ID ASC (optional)
                 .ToListAsync();
